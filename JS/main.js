d21fb5183ee9858d7de97aed6b4707f3ae9a4924
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-05-26 08:25:32 -0300

    Index & sobre| Link javascript

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+console.log("Teste conexão do JavaScript...");
+console.log("JavaScript conectado!!");
+document.querySelector("main").addEventListener("dblclick", function(){alert("A página foi clicada!")});
+
