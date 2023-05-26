bf5aaf6c4e0b256e71f57a2437788e3b59632add
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-05-26 08:34:58 -0300

    Testes

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,8 @@
 console.log("Teste conexão do JavaScript...");
 console.log("JavaScript conectado!!");
-document.querySelector("main").addEventListener("dblclick", geraAlertaDeClick());
-
-function geraAlertaDeClick(){
+document.querySelector("main").addEventListener("dblclick", function(){
     alert("A página foi clicada!!");
     console.log("Clicou!!");
-}
+});
+
 
